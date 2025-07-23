3dcc0fb9e52d4ede8270de8427dfd6dc23f94e40
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2025-07-23 22:17:08 +0500

    [master] highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.tBodies[0].children;
+  
+  for (let row of rows) {   
+    rowHandler(row);
+  }
+}
+
+function rowHandler(row) {
+  const cells = row.cells;
+
+  for (let i = 0; i <= cells.length; i++) {
+    const cell = cells[i];
+
+    //status handler
+    if (i === 3) {
+      if (cell.hasAttribute('data-available')) {
+        const availableClass = cell.dataset.available === 'true' ? 'available' : 'unavailable';
+        row.classList.add(availableClass);
+      } else {
+        row.hidden = true;
+      }
+    } else if (i === 2) { // gender handler
+      const genderClass = cell.innerHTML === 'm' ? 'male' : 'female';
+      row.classList.add(genderClass);
+    } else if (i === 1) { // age handler
+      const under18 = cell.innerHTML < 18;
+      if (under18) {
+        row.style = "text-decoration: line-through";
+      }
+    }
+  }
 }
