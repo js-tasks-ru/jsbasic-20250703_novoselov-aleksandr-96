af0bf120b0c8e302995965b2efa8f7ed413ebec1
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2025-08-17 12:50:39 +0500

    [master] копируем task 3

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,76 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
+  #steps = 0
+  #value = 0
+  elem = null
+  #oldActiveIndex = 0
+  #allSliderSpan = null
+
   constructor({ steps, value = 0 }) {
+    this.#steps = steps;
+    this.#value = value;
+    this.#oldActiveIndex = value;
+
+    this.#render();
+  }
+
+  #html() {
+    return `
+      <div class="slider">
+        <!--Ползунок слайдера с активным значением-->
+        <div class="slider__thumb">
+          <span class="slider__value">0</span>
+        </div>
+
+        <!--Полоска слайдера-->
+        <div class="slider__progress"></div>
+
+        <!-- Шаги слайдера (вертикальные чёрточки) -->
+        <div class="slider__steps">
+          ${Array(this.#steps)
+          .fill('<span></span>')
+          .join('\n')}
+        </div>
+      </div>
+    `;
+  }
+
+  #render() {
+    this.elem = createElement(this.#html());
+
+    this.allSliderSpan = Array.from(this.elem.querySelectorAll('span')).filter(elem => !elem.classList.contains('slider__value'));
+    this.allSliderSpan[this.#value].classList.toggle('slider__step-active');
+
+    
+    this.elem.addEventListener('click', event => {
+      let left = Math.abs(event.clientX - this.elem.getBoundingClientRect().left);
+      let leftRelative = left / this.elem.offsetWidth;
+      let segments = this.#steps - 1;
+      let approximateValue = leftRelative * segments;
+      let value = Math.round(approximateValue);
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+
+      this.#value = value;
+      
+      this.elem.querySelector('.slider__value').innerHTML = this.#value;
+      
+      this.allSliderSpan[this.#oldActiveIndex].classList.toggle('slider__step-active');
+      this.allSliderSpan[this.#value].classList.toggle('slider__step-active');
+      this.#oldActiveIndex = this.#value;
+  
+      let valuePercents = this.#value / segments * 100;
+
+      thumb.style.left = `${valuePercents}%`;
+      progress.style.width = `${valuePercents}%`;
+
+      const sliderChangeEvent = new CustomEvent('slider-change', { // имя события должно быть именно 'slider-change'
+        detail: this.#value, // значение 0, 1, 2, 3, 4
+        bubbles: true // событие всплывает - это понадобится в дальнейшем
+      });
+
+      this.elem.dispatchEvent(sliderChangeEvent);
+    });
   }
 }
